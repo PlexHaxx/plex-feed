1b2b01d93b45036ef653f9e4fea0fe43529b2355
Steve Barrau <steve.barrau@epita.fr>
2015-10-06 01:11:29 +0200

    Add threading and fix ordering

--- plex-feed/__init__.py
+++ plex-feed/__init__.py
@@ -1,74 +1,57 @@
 #!/usr/bin/env python3
 
-import sys
-import bisect
-import datetime
+import concurrent.futures
 
 import pytz
 from plexapi.myplex import MyPlexUser
-from clint.textui import colored
 from feedgen.feed import FeedGenerator
 
 
-class MyResource:
-    def __init__(self, title, server, section, date):
-        self.title = title
-        self.server = server
-        self.section = section
-        self.date = date
+data = []
 
-    def __lt__(self, other):
-        return self.date < other.date
+
+def parse_resource(user, resource):
+    for section in user.getResource(resource.name).connect().library.sections():
+        for video in section.recentlyAdded():
+            if video.lastViewedAt:
+                continue
+            data.append(video)
+    return True
 
 
 def main():
-    data = []
     config = __import__('config')
     user = MyPlexUser.signin(config.username, config.password)
 
-    for resource in user.resources():
-        plex = user.getResource(resource.name).connect()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        for resource in user.resources():
+            executor.submit(parse_resource, user, resource)
 
-        for section in plex.library.sections():
-            for video in section.recentlyAdded():
-                if video.lastViewedAt:
-                    print(colored.red('X'), end='')
-                    sys.stdout.flush()
-                    continue
-
-                d = MyResource(
-                    video.title, resource.name, section.title,
-                    pytz.timezone('Europe/Paris').localize(resource.createdAt)
-                )
-                bisect.insort(data, d)
-                print(colored.green('.'), end='')
-                sys.stdout.flush()
-        print()
-
-    ordered = reversed(data)
-    print(len(data))
+    print('Servers data parsed')
+    paris = pytz.timezone('Europe/Paris')
+    ordered = reversed(sorted(data, key=lambda x: paris.localize(x.addedAt)))
+    print('Media sorted')
 
     fg = FeedGenerator()
     fg.id('http://satreix.fr/feeds/plex.rss')
+    fg.generator('plex-feed')
     fg.title('PLEX feed')
+    fg.subtitle('Newly added media content')
     fg.author({'name': 'satreix', 'email': 'satreix@gmail.com'})
     fg.link(href='http://satreix.fr', rel='alternate')
     fg.logo('https://plex.tv/assets/img/googleplus-photo-cb6f717c8cfd8b48df6dbb09aa369198.png')
-    fg.subtitle('Newly added media content')
     fg.link(href='http://satreix.fr/feeds/plex.rss', rel='self')
     fg.language('en')
 
-    for elt in ordered:
-        # print('- {} {}'.format(elt.date, elt.title))
+    for elt in list(ordered)[:50]:
         fe = fg.add_entry()
-        # fe.id('http://lernfunk.de/media/654321/1')
+        fe.id(elt.getStreamUrl())
         fe.title(elt.title)
-        fe.pubdate(elt.date)
-        # fe.description(elt.server)
-        # fe.category(elt.section)
+        fe.pubdate(paris.localize(elt.addedAt))
+        fe.description(elt.summary, True)
 
-    fg.rss_file('plex.rss')
-    print('File wrote')
+    fg.rss_file('plex.rss', pretty=True)
+    print('File written')
 
 
 if __name__ == '__main__':
